be9626972589c8c89f29e439f9a627cd65a88bd1
Reyna <ranakara2005@gmail.com>
2024-05-10 15:37:48 +0300

    pls work

--- focusscript.js
+++ focusscript.js
@@ -422,4 +422,37 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    // Get the fullscreen button element
+    const fullscreenButton = document.getElementById('full-screen');
+
+    // Add click event listener to the fullscreen button
+    fullscreenButton.addEventListener('click', toggleFullScreen);
+
+    // Function to toggle fullscreen mode
+    function toggleFullScreen() {
+        if (!document.fullscreenElement) {
+            // If the document is not in fullscreen mode, request fullscreen
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+                document.documentElement.webkitRequestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
+                document.documentElement.msRequestFullscreen();
+            }
+        } else {
+            // If the document is already in fullscreen mode, exit fullscreen
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) { /* Firefox */
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { /* IE/Edge */
+                document.msExitFullscreen();
+            }
+        }
+    }
+
 });
