355ff80e25917150c4f9e4476adf0337c7ca13c9
Erin C <160546107+777ParallelClematis@users.noreply.github.com>
2026-02-01 07:33:21 +1300

    GOSSTag to GOSStag

--- goss-mvp/src/components/Navbar.jsx
+++ goss-mvp/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import GOSSTag from "../assets/GOSSTag.png";
+import GOSSTag from "../assets/GOSStag.png";
 
 export default function Navbar() {
   return (
