f244e56ac08cd5110a1356ca4ea8abf02d974ed5
Shanine Gmyrek <shanine.gmyrek@sap.com>
2026-03-04 10:31:58 +0100

    Remove Hardcoded Pics and add new arrow

--- goss-mvp/src/components/Hero.jsx
+++ goss-mvp/src/components/Hero.jsx
@@ -25,16 +25,17 @@ export default function Hero() {
             rel="noreferrer"
             aria-label="Join the Girls Only Study Space"
           >
-            <span>Join GOSS → </span>
+            <span>Join GOSS </span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24" width="24">
+              <g id="arrow-right-keyboard">
+                <path fill="#F6F1DB" fill-rule="evenodd" d="m23.1173 11.1162 -8 -7.99999 -1.7678 1.76777 5.8661 5.86612 -19.214623437 0 0 2.5 19.214623437 0 -5.8661 5.8661 1.7678 1.7678 8 -8 0.8839 -0.8839 -0.8839 -0.8839Z" clip-rule="evenodd" stroke-width="1"></path>
+              </g>
+            </svg>
           </a>
 
           <BenefitsStrip />
         </div>
       </div>
-
-      <img src={crown} className="picBg crown" alt="Crown" />
-      <img src={star} className="picBg star" alt="Star" />
-
       <SocialLinks />
     </section>
   );
