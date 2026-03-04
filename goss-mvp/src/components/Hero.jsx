8e011b00b25f89d1601d1574a9aaf33c6b37f794
Shanine Gmyrek <shanine.gmyrek@sap.com>
2026-03-04 17:34:06 +0100

    remove Imports

--- goss-mvp/src/components/Hero.jsx
+++ goss-mvp/src/components/Hero.jsx
@@ -1,8 +1,6 @@
 import WordCarousel from "./WordCarousel.jsx";
 import { HERO_WORDS } from "../data/heroWords.js";
 import SocialLinks from "./SocialLinks.jsx";
-import crown from "../assets/doodles/crown.png";
-import star from "../assets/doodles/star.png";
 import BenefitsStrip from "./BenefitsStrip.jsx";
 import HeroBackground from "./HeroBackground.jsx";
 
